9eb494140e6f559327f61504b2b6b491dcc9be40
Andy Krause <andy.krause@unimelb.edu.au>
2016-11-16 19:37:33 +1100

    calculating revenues

--- abbReturns_Working.R
+++ abbReturns_Working.R
@@ -40,65 +40,80 @@
 
 ### Working analysis ---------------------------------------------------------------------  
 
+ ## Impute prices
+
+  # Before imputing the rents and rates, we fix the product factors so that the 
+   # models use a 2bed/2bath unit as the control unit against which others are 
+   # compared.  We also de-factorize the suburb variable for easier use in the 
+   # regression models that follow. 
+
+ ## Re-order factors
+
+  abb.dataf$bedbath <- factor(abb.dataf$bedbath, 
+                              levels=c('2..2', '1..1', '2..1', '3..1', '3..2', '4..2'))
+  rent.dataf$bedbath <- factor(rent.dataf$bedbath, 
+                               levels=c('2..2', '1..1', '2..1', '3..1', '3..2', '4..2'))
+
+ ## Add product as a variable
+  
+  abb.dataf$product <- paste0(substr(abb.dataf$type, 1, 1), abb.dataf$bedbath)
+  rent.dataf$product <- paste0(substr(rent.dataf$type, 1, 1), rent.dataf$bedbath)
+
+ ## Convert suburbs from factor to character
+
+  abb.dataf$suburb <- as.character(abb.dataf$suburb)
+  rent.dataf$suburb <- as.character(rent.dataf$suburb)
+
+ ## Global model
+
+  # We begin by imputing rents and rates for all properties using a global model with 
+  # suburb fixed effects.  
+ 
+  # Set model specifications
+  rent.mod.spec <- formula(log(event.price) ~ as.factor(bedbath) + as.factor(suburb))
+  abb.mod.spec <- formula(log(nightly.rate) ~ as.factor(bedbath) + as.factor(suburb))
+ 
+ ## Impute globally 
+
+  glob.imp <- crossImputeModel(rent.df=rent.dataf, 
+                               abb.df=abb.dataf, 
+                               rent.mod.spec=rent.mod.spec, 
+                               abb.mod.spec=abb.mod.spec,
+                               clip.field='suburb')
+  
+ ## Calculate market occ and rate quantiles
+  
+  # Split into markets
+  abb.mrkt <- split(glob.imp$abb, glob.imp$abb$product)
+  rent.mrkt <- split(glob.imp$rent, glob.imp$rent$product)
+  
+  # Calculate AirBNB Quantiles
+  abb.qtls <- lapply(abb.mrkt, calcOPQuantiles, daily.df=daily.dataf)
+  rent.qtls <- lapply(rent.mrkt, calcLTQuantiles)  
+  
+ ## Calculate the revenues
+  
+  rev.list <- mapply(calcRevenues, 
+                     abb.qtls=abb.qtls, 
+                     rent.qtls=rent.qtls,
+                     SIMPLIFY=FALSE,
+                     MoreArgs=list(mrkt.occ=TRUE,
+                                   mrkt='sub.mrkt'))
+  all.revs <- rbind.fill(rev.list)
+  
+  
+  
+
+  
+## Suburb specific occ.rates
+  
+  aa <- rbind.fill(lapply(abb.qtls, function(x) x$abb.df))
   
   
   
   
-    
-## Impute prices
-
-#Before imputing the rents and rates, we fix the product factors so that the 
-  #models use a 2bed/2bath unit as the control unit against which others are 
-  #compared.  We also de-factorize the suburb variable for easier use in the 
-  #regression models that follow. 
-
+  
  
-## Re-order factors
-
-abb.dataf$bedbath <- factor(abb.dataf$bedbath, 
-                            levels=c('2..2', '1..1', '2..1', '3..1', '3..2', '4..2'))
-rent.dataf$bedbath <- factor(rent.dataf$bedbath, 
-                             levels=c('2..2', '1..1', '2..1', '3..1', '3..2', '4..2'))
-
-## Convert suburbs from factor to character
-
-abb.dataf$suburb <- as.character(abb.dataf$suburb)
-rent.dataf$suburb <- as.character(rent.dataf$suburb)
-
- 
-### Global model
-
-#We begin by imputing rents and rates for all properties using a global model with 
-#suburb fixed effects.  
- 
-## Set model specifications
-
-rent.mod.spec <- formula(log(event.price) ~ as.factor(bedbath) + as.factor(suburb))
-abb.mod.spec <- formula(log(nightly.rate) ~ as.factor(bedbath) + as.factor(suburb))
- 
-## Impute globally 
-
-glob.imp <- crossImputeModel(rent.dataf, 
-                             abb.dataf, 
-                             rent.mod.spec=rent.mod.spec, 
-                             abb.mod.spec=abb.mod.spec,
-                             clip.field='suburb')
-
-#We then calculate the gross hypothetical returns by multiplying the imputed rents 
-#and rates by months and days, respectively. After this we must substract lost income 
-#due to vacancy and due to costs.
-#
-#For long-term rentals the primary lost income due to vacny is 
-
-abb.df <- glob.imp$abb
-abb.df$product <- paste0(substr(abb.df$type, 1, 1), abb.df$bedbath)
-
-xx <- split(abb.df, abb.df$product)
-
-sm.df <- xx[[1]]
-
-g <- lapply(xx, calcOPQuantiles, daily.df=daily.dataf)
-gg <- calcOPQuantiles(abb.df, daily.df=daily.dataf)
 
 #   ggplot(data = ss,
 #             aes(x = occ.rate,
