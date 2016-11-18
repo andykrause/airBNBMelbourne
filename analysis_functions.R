dde7952754f18d013febbf0bafcc6b35099e9c62
Andy Krause <andy.krause@unimelb.edu.au>
2016-11-18 20:13:54 +1100

    Reorganization/winnowing of functoins

--- analysis_functions.R
+++ analysis_functions.R
@@ -3,9 +3,8 @@ crossImputeModel <- function(rent.df,
                              abb.df,
                              rent.mod.spec,
                              abb.mod.spec,
-                             geo.field=NULL,
-                             geo.value=NULL,
-                             clip.field=NULL)
+                             exch.rate,
+                             clip.fields=NULL)
 {
   
  ## Arguments
@@ -14,193 +13,114 @@ crossImputeModel <- function(rent.df,
   # abb.df:  data.frame of airbnb properties
   # rent.mod.spec:  specification for rent price model
   # abb.mod.spec:  specification for airbnb properties
-  # geo.field: field to split submarkets by
-  # geo.value: specific geography to use
   # clip.field: field to ensure factors match between rent and abb
-  
- ## Extract Data from geo
-  
-  if(!is.null(geo.field)){
-    
-    # Create IDs for inclusion in the geo
-    id.rgeo <- which(rent.df[,geo.field] == geo.value)
-    id.ageo <- which(abb.df[,geo.field] == geo.value)
-    
-    # Extract and create new DFs
-    rent.df <- rent.df[id.rgeo, ]
-    abb.df <- abb.df[id.ageo, ]
-    
-  }
-  
- ## Split data by Property Type  
-  
-  rent.house <- rent.df[rent.df$type == 'House', ]
-  rent.apt <- rent.df[rent.df$type == 'Apartment', ]
-  abb.house <- abb.df[abb.df$type == 'House', ]
-  abb.apt <- abb.df[abb.df$type == 'Apartment', ]
   
  ## Remove those within the clip field that isn't present in both  
   
-  if(!is.null(clip.field)){
+  if(!is.null(clip.fields)){
     
-    # Find the fields that are used to clip
-    r.cf <- which(names(rent.df) == clip.field)
-    a.cf <- which(names(abb.df) == clip.field)
+    for(i.cf in 1:length(clip.fields)){
     
-    # Get IDs for those to be removed
-    id.rh <- rent.house[ ,r.cf] %in% names(table(as.character(abb.house[ ,a.cf])))
-    id.ra <- rent.apt[ ,r.cf] %in% names(table(as.character(abb.apt[ ,a.cf])))
-    id.ah <- abb.house[ ,a.cf] %in% names(table(as.character(rent.house[ ,r.cf])))
-    id.aa <- abb.apt[ ,a.cf] %in% names(table(as.character(rent.apt[ ,r.cf])))
+      # Find the fields that are used to clip
+      r.cf <- which(names(rent.df) == clip.fields[i.cf])
+      a.cf <- which(names(abb.df) == clip.fields[i.cf])
     
-    # Filter out obs missing matched factors  
-    rent.house <- rent.house[id.rh, ]
-    rent.apt <- rent.apt[id.ra, ]
-    abb.house <- abb.house[id.ah, ]
-    abb.apt <- abb.apt[id.aa, ]
-  
-  }
+      # Get IDs for those to be removed
+      id.r <- rent.df[ ,r.cf] %in% names(table(as.character(abb.df[ ,a.cf])))
+      id.a <- abb.df[ ,a.cf] %in% names(table(as.character(rent.df[ ,r.cf])))
+    
+      # Filter out obs missing matched factors  
+      rent.df <- rent.df[id.r, ]
+      abb.df <- abb.df[id.a, ]
+    
+    }
+    
+ }
   
  ## Build regression models for rental values
   
-  rh.mod <- lm(rent.mod.spec, data=rent.house)
-  ra.mod <- lm(rent.mod.spec, data=rent.apt)
-  
+  r.mod <- lm(rent.mod.spec, data=rent.df)
+
  ## Add the fitted values to the long term data
   
-  rent.house$imp.rent <- exp(rh.mod$fitted)
-  rent.apt$imp.rent <- exp(ra.mod$fitted)
-  
+  rent.df$imp.rent <- exp(r.mod$fitted)
+
  ## Add the predicted values to the short term data
   
-  abb.house$imp.rent <- exp(predict(rh.mod, abb.house))
-  abb.apt$imp.rent <- exp(predict(ra.mod, abb.apt))
-  
+  abb.df$imp.rent <- exp(predict(r.mod, abb.df))
+
  ## Build regression models
   
-  ah.mod <- lm(abb.mod.spec, data=abb.house)
-  aa.mod <- lm(abb.mod.spec, data=abb.apt)
-  
+  a.mod <- lm(abb.mod.spec, data=abb.df)
+
  ## Add the fitted values to the long term data
   
-  abb.house$imp.rate <- exp(ah.mod$fitted)
-  abb.apt$imp.rate <- exp(aa.mod$fitted)
-  
+  abb.df$imp.rate <- exp(a.mod$fitted)
+
  ## Add the predicted values to the short term data
   
-  rent.house$imp.rate <- exp(predict(ah.mod, rent.house))
-  rent.apt$imp.rate <- exp(predict(aa.mod, rent.apt))
-  
- ### Merge back together
-  
-  abb.data <- rbind(abb.house, abb.apt)
-  rent.data <- rbind(rent.house, rent.apt)
-  
- ## Estimate Max Revenue
- 
-  abb.data$imprent.revenue <- abb.data$imp.rent * 52
-  abb.data$imprate.revenue <- abb.data$imp.rate * 365
-  
-  rent.data$imprent.revenue <- rent.data$imp.rent * 52
-  rent.data$imprate.revenue <- rent.data$imp.rate * 365
-  
+  rent.df$imp.rate <- exp(predict(a.mod, rent.df)) * exch.rate
+
  ## Return Values  
   
-  return(list(abb=abb.data,
-              rent=rent.data,
-              abb.h.mod=ah.mod,
-              abb.a.mod=aa.mod,
-              rent.h.mod=rh.mod,
-              rent.a.mod=ra.mod))
+  return(list(abb=abb.df,
+              rent=rent.df,
+              abb.mod=a.mod,
+              rent.mod=r.mod))
 }
 
+### Calculate the Airbnb quantiles -------------------------------------------------------
 
-
-
-
-
-
-
-
-
-
-
-
-calcOPQuantiles <- function(sm.df,
-                            daily.df){
+calcABBQtls <- function(abb.df){
   
-  ## Extract limited daily data  
-  
-  daily.sm <- daily.df[daily.df$property.id %in% sm.df$property.id, ]  
-  
-  ## Calculate the number of open days  
-  
-  # Table all status types
-  status.table <- as.data.frame(table(daily.sm$status, daily.sm$property.id))
-  
-  # cast into a wide data frame
-  status.df <- dcast(status.table, Var2 ~ Var1)
-  
-  # All all.days and open days values
-  status.df$all.days <- rowSums(status.df[ ,2:4])
-  status.df$open.days <- status.df$all.days - status.df$B
-  
-  # Remove those with no open days or no reservations
-  #status.df <- status.df[status.df$open.days != 0, ]
-  #status.df <- status.df[status.df$R != 0, ]
-  
- ## Calculate occupancy rates  
-  
-  # Calc property specific rate
-  status.df$occ.rate <- status.df$R / status.df$open.days
-  status.df$occ.rate[is.na(status.df$occ.rate)] <- 0
-  status.df$occ.rate[!is.finite(status.df$occ.rate)] <- 0
-
-  # Set up clean set for computation
-  status.comp <- status.df[!is.na(status.df$occ.rate), ]
-  status.comp <- status.comp[status.comp$occ.rate > 0, ]
-
-  # Quantiles for submarket occupancy rates
-  occ.qtl <- Hmisc::wtd.quantile(status.comp$occ.rate, 
-                                 weights=status.comp$open.days, 
+ # Quantiles for market occupancy rates (with a small jitter)
+ 
+  occ.qtl <- Hmisc::wtd.quantile(abb.df$occ.rate + runif(nrow(abb.df), -.0001, .0001), 
+                                 weights=(abb.df$bookings + abb.df$available.days), 
                                  probs=seq(0, 1, .01))
-  occ.qtl[[1]] <- -101/10000000
+
+ # Quantile for market nightly rates
   
-  ## Calculate rate quantiles
+  rate.qtl <- Hmisc::wtd.quantile(abb.df$med.rate + runif(nrow(abb.df), -1, 1), 
+                                  weights=(abb.df$bookings + abb.df$available.days), 
+                                  probs=seq(0, 1, .01))
   
-  # Limit to non-blocked days
-  open.daily <- daily.sm[daily.sm$status != 'B', ]
+ # Add quantile ranking to data
+  abb.df$occ.qtl <- as.numeric(as.factor(cut(abb.df$occ.rate, 
+                                             breaks=(occ.qtl + (1:101/1000000)))))
   
-  # Calculate quantiles
-  rate.qtl <- quantile(open.daily$price, probs=seq(0, 1, .01))
+ # Add quantile ranking to data
+  abb.df$rate.qtl <- as.numeric(as.factor(cut(abb.df$med.rate, 
+                                               breaks=(rate.qtl + (1:101/1000000)))))
   
- ## Add to sm data.frame
-  
-  # Add occupancy rate
-  sm.df$occ.rate <- status.df$occ.rate[match(sm.df$property.id, status.df$Var2)]
-  sm.df$occ.qtl <- as.numeric(as.factor(cut(sm.df$occ.rate, 
-                                            breaks=(occ.qtl + (1:101/10000000)))))
-  sm.df$rate.qtl <- as.numeric(as.factor(cut(sm.df$nightly.rate, 
-                                             breaks=(rate.qtl + (1:101/100000)))))
-  
-  ## Return Values
-  
-  return(list(abb.df=sm.df,
+ return(list(abb.df=abb.df,
               occ.qtl=occ.qtl,
               rate.qtl=rate.qtl))
 }
 
 ### Calculate the quantiles (rent and DOM) for the long term rentals ---------------------
 
-calcLTQuantiles <- function(rent.df){
+calcRentQtls <- function(rent.df){
   
  ## Calc the rent quantiles
   
-  rent.qtl <- quantile(rent.df$event.price, probs=seq(0, 1, 0.01))
+  rent.qtl <- quantile(rent.df$event.price, 
+                       probs=seq(0, 1, 0.01)) 
   
  ## Days on market quantiles  
   
-  dom.qtl <- quantile(rent.df$dom, probs=seq(0, 1, 0.01))
+  dom.qtl <- quantile(rent.df$dom + runif(nrow(rent.df), -.1, .1), 
+                      probs=seq(0, 1, 0.01))  
+  
+ ## Add quantile ranking to data
+  
+  rent.df$dom.qtl <- as.numeric(as.factor(cut(rent.df$dom, 
+                                              breaks=(dom.qtl + (1:101/1000000)))))
+  
+ ## Add quantile ranking to data
+  
+  rent.df$rent.qtl <- as.numeric(as.factor(cut(rent.df$event.price, 
+                                               breaks=(rent.qtl + (1:101/1000000)))))
   
  ## Return values  
   
@@ -211,17 +131,17 @@ calcLTQuantiles <- function(rent.df){
 
 ### Calculate the revenues for each product mix ------------------------------------------
 
-calcRevenues <- function(abb.qtls,
-                         rent.qtls,
-                         mrkt.occ=FALSE,
-                         mrkt=NULL){
+imputeRevenues <- function(abb.qtls,
+                           rent.qtls,
+                           mrkt.occ=FALSE,
+                           mrkt=NULL){
   
-  ## Strip out property data  
+ ## Strip out property data  
   
   abb.df <- abb.qtls$abb.df
   rent.df <- rent.qtls$rent.df
   
-  ## Strip out quantile data  
+ ## Strip out quantile data  
   
   occ.qtl <- abb.qtls$occ.qtl
   dom.qtl <- rent.qtls$dom.qtl
@@ -243,22 +163,20 @@ calcRevenues <- function(abb.qtls,
                                                    market.df$market)]
   }
   
-  ## Estimate airbnb properties (rents and rates)  
+ ## Estimate airbnb properties (rents and rates)  
   
   abb.df$rent.rev <- abb.df$imp.rent * (52 - (dom.qtl[51]/7))
   
   if(mrkt.occ){
     abb.df$abb.rev <- abb.df$imp.rate * 365 * abb.df$market.occ
-    abb.df$abb.revx <- abb.df$nightly.rate * 365 * abb.df$market.occ
   } else {
     abb.df$abb.rev <- abb.df$imp.rate * 365 * occ.qtl[51]
-    abb.df$abb.revx <- abb.df$nightly.rate * 365 * abb.df$occ.rate
   }  
   
-  ## Estimate long term properties (rent and rates)  
+ ## Estimate long term properties (rent and rates)  
   
   rent.df$rent.rev <- rent.df$imp.rent * (52 - (dom.qtl[51]/7))
-  rent.df$rent.revx <- rent.df$event.price * (52 - (rent.df$dom/7))
+  
   if(mrkt.occ){
     rent.df$abb.rev <- rent.df$imp.rate * 365 * rent.df$market.occ
   } else {
@@ -267,42 +185,152 @@ calcRevenues <- function(abb.qtls,
   
  ## Save for future cost estimates
 
-
- ## Calculate abb premium
+ ## Return Values
   
-  abb.df$abb.prem <- abb.df$abb.rev - abb.df$rent.rev
-  abb.df$abb.prem.act <- abb.df$abb.revx - abb.df$rent.rev
-  rent.df$abb.prem <- rent.df$abb.rev - rent.df$rent.rev
-  rent.df$abb.prem.act <- rent.df$abb.rev - rent.df$rent.revx
-
- ## Combine
-  
-  # Prepare ABB data
-  abb.comb <- abb.df[, c('property.id', 'sub.mrkt', 'product', 'suburb',
-                         'imp.rent', 'imp.rate', 'rent.rev', 'abb.rev',
-                         'latitude', 'longitude',
-                         'abb.revx', 'nightly.rate',
-                         'abb.prem', 'abb.prem.act')]
-  names(abb.comb)[c(1,11,12)] <- c('id', 'act.rev','act.rr')
-  abb.comb$id <- paste0('abb.', abb.comb$id)
-  abb.comb$data <- 'abb'
-
-  # Prepare rent data
-  rent.comb <- rent.df[, c('id.key', 'sub.mrkt', 'product', 'suburb',
-                           'imp.rent', 'imp.rate', 'rent.rev', 'abb.rev',
-                           'latitude', 'longitude',
-                           'rent.revx', 'event.price',
-                           'abb.prem', 'abb.prem.act')]
-  names(rent.comb)[c(1,11,12)] <- c('id', 'act.rev','act.rr')
-  rent.comb$id <- paste0('rent.', rent.comb$id)
-  rent.comb$data <- 'rent'
-  
-  # Combine
-  all.comb <- rbind(abb.comb, rent.comb)
-  all.comb$abb.better <- ifelse(all.comb$abb.prem > 0, 1, 0)
-  
- ## Return values
-  
-  return(all.comb)
-  
+  return(list(abb=abb.df,
+              rent=rent.df))
 }
+
+### Full return comparison function ------------------------------------------------------
+
+compReturns <- function(rent.df=rent.dataf, 
+                        abb.df=abb.dataf, 
+                        rent.mod.spec=rent.mod.spec, 
+                        abb.mod.spec=abb.mod.spec,
+                        exch.rate=exch.rate,
+                        comp.field='sub.mrkt',
+                        clip.fields='suburb'){  
+  
+ ## Arguments
+  
+  # rent.df: data.frame of rental obs
+  # abb.df: data.frame of airbnb obs
+  # rent.mod.spec: rental data model specification
+  # abb.mod.spec: airbnb model specification
+  # exch.rate: A$ in US$
+  # clip.fields: fields that are factors in the model specifications
+  
+ ## Impute rates and rents 
+  
+  imp.data <- crossImputeModel(rent.df=rent.df, 
+                               abb.df=abb.df, 
+                               rent.mod.spec=rent.mod.spec, 
+                               abb.mod.spec=abb.mod.spec,
+                               exch.rate=exch.rate,
+                               clip.fields=clip.fields)
+  
+ ## Calculate market occ and rate quantiles
+  
+  # Calculate Rate/Rent/OccRate/DOM Quantiles
+  abb.qtls <- calcABBQtls(imp.data$abb)
+  rent.qtls <- calcRentQtls(imp.data$rent)  
+  
+ ## Calculate the revenues
+  
+  rev.data <- imputeRevenues(abb.qtls=abb.qtls, 
+                             rent.qtls=rent.qtls)
+  
+ ## Compare ABB to Rent
+  
+  # Abb
+  abb.df <-rev.data$abb
+  abb.df$abb.prem <- abb.df$abb.rev - abb.df$rent.rev
+  abb.df$abb.prem.act <- abb.df$revenue - abb.df$rent.rev
+  abb.df$abb <- ifelse(abb.df$abb.prem > 0, 1, 0)
+  abb.df$abb.act <- ifelse(abb.df$abb.prem.act > 0, 1, 0)
+  
+  # Rent
+  rent.df <-rev.data$rent
+  rent.df$abb.prem <- rent.df$abb.rev - rent.df$rent.rev
+  rent.df$abb.prem.act <- rent.df$abb.rev - rent.df$revenue
+  rent.df$abb <- ifelse(rent.df$abb.prem > 0, 1, 0)
+  rent.df$abb.act <- ifelse(rent.df$abb.prem.act > 0, 1, 0)
+  
+  # Make table of comparisons
+  abb.imp <- tapply2DF(abb.df$abb, abb.df[,comp.field], mean)
+  abb.act <- tapply2DF(abb.df$abb.act, abb.df[,comp.field], mean)
+  rent.imp <- tapply2DF(rent.df$abb, rent.df[,comp.field], mean)
+  rent.act <- tapply2DF(rent.df$abb.act, rent.df[,comp.field], mean)
+  abb.imp$est <- rent.imp$est <- 'imputed'
+  abb.imp$data <- abb.act$data <- 'abb'
+  rent.act$est <- abb.act$est <- 'actual'
+  rent.act$data <- rent.imp$data <- 'rent'
+  rate.table <- rbind(abb.imp, abb.act, rent.imp, rent.act)
+  
+ ## Plot comparisons
+  
+  # Across all data and est
+  rate.plot <- ggplot(rate.table, aes(x=ID, weights=Var)) + geom_bar() +
+    facet_grid(data ~ est)
+  
+  # Just actual AirBNB
+  act.plot <- ggplot(rate.table[rate.table$data == 'abb' & rate.table$est == 'actual', ],
+                     aes(x=ID, weights=Var)) + 
+    geom_bar() 
+  
+  
+  q.list <- list()
+  abb.df$oc <- round(abb.df$occ.rate, 2)
+  for(q in 1:100){
+    
+    aa <- abb.df[abb.df$oc == q/100, ]
+    x.act <- tapply2DF(aa$abb.act, aa$sub.mrkt, mean)
+    x.act$qtl <- q
+    q.list[[q]] <- x.act
+    
+  }
+  
+  jj <- rbind.fill(q.list)
+  occ.plot <- ggplot(jj, aes(x=qtl, y=Var, group=ID, color=ID)) + 
+    geom_point() + 
+    stat_smooth(se=F,span=.66, n=222)
+  
+  
+  q.list <- list()
+  #abb.df$oc <- round(abb.df$occ.rate, 2)
+  for(q in 1:100){
+    
+    aa <- abb.df[abb.df$rate.qtl == q, ]
+    x.act <- tapply2DF(aa$abb.act, aa$sub.mrkt, mean)
+    x.act$qtl <- q
+    q.list[[q]] <- x.act
+    
+  }
+  
+  jj <- rbind.fill(q.list)
+  price.plot <- ggplot(jj, aes(x=qtl, y=Var, group=ID, color=ID)) + 
+    geom_point() + 
+    stat_smooth(se=F,span=.66, n=222)
+  
+  point.plot <- ggplot(data = abb.df,
+                       aes(x = occ.qtl,
+                           y = rate.qtl,
+                           color=as.factor(abb.act))) +
+    geom_point()
+  
+  heat.map <- ggplot(data = abb.df,
+                     aes(x = occ.qtl,
+                         y = rate.qtl)) + 
+    stat_bin2d(data=abb.df, aes(alpha=..count.., fill=as.factor(abb.act)),
+               binwidth=c(10, 10))
+  
+  a<- table(paste0(round(abb.df$rate.qtl, -1), 
+                   ".", 
+                   round(abb.df$occ.qtl, -1)),
+            abb.df$abb.act)
+  c<-(a[,2]-a[,1])
+  ab <- length(which(c>0))/length(c)
+  re <- length(which(c<0))/length(c)
+  market.value <- ab/re
+  
+  return(list(market.value=market.value,
+              abb=abb.df,
+              rent=rent.df,
+              rate.plot=rate.plot,
+              occ.plot=occ.plot,
+              point.plot=point.plot,
+              price.plot=price.plot,
+              heat.map=heat.map
+  ))
+  
+}   
