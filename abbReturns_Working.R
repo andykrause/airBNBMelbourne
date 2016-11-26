bd5fb609a195131e11221abe230c7c177ecb6704
Andy Krause <andy.krause@unimelb.edu.au>
2016-11-27 09:47:39 +1100

    remove data cleaning

--- abbReturns_Working.R
+++ abbReturns_Working.R
@@ -55,70 +55,7 @@
   
 ### Working analysis ---------------------------------------------------------------------  
 
-  exch.rate <- 1.32
 
- ## Calculate actual revenue for study period for AirBnb
-  
-  # Determine # of booking for each airbnb
-  abb.dataf$bookings <- (abb.dataf$total.days - 
-                          abb.dataf$blocked.days - 
-                           abb.dataf$available.days)
-  
-  # Determine property specific occupancy rate
-  abb.dataf$occ.rate <- (abb.dataf$bookings / 
-                           (abb.dataf$total.days - abb.dataf$blocked.days))
-  
-  # Set the blocked rate
-  abb.dataf$blocked.rate <- abb.dataf$blocked.days / abb.dataf$total.days
-  
-  # Set the extrapolation parameter for those on the site for less than whole period
-  abb.dataf$extr.par <- 366 / abb.dataf$total.days
-  
-  # Get all of the reservations
-  resv.daily <- daily.dataf[daily.dataf$status == 'R' &
-                             daily.dataf$booked.date != 'imputed' &
-                              daily.dataf$price < 600, ]
-  
-  # Calculate the property specific median nightly rate
-  med.daily <- tapply2DF(resv.daily$price, resv.daily$property.id, median)
-  
-  # Add median rate to the property  
-  abb.dataf$med.rate <- med.daily$Var[match(abb.dataf$property.id, med.daily$ID)] 
-  
-  # Calculate the total revenue  
-  abb.dataf$revenue <- (abb.dataf$med.rate * exch.rate * 
-                          abb.dataf$extr.par * abb.dataf$bookings)
-  
-  # Remove those with no revenue
-  abb.dataf <- abb.dataf[!is.na(abb.dataf$revenue), ]
-  
- ## Calculate ltral revenues  
-  
-  # Calculate the actual revenue
-  ltr.dataf$revenue <- ltr.dataf$event.price * (52 - ltr.dataf$dom/7)
-  
-  # Remove those with very low revenues (due to very long DOMs)
-  ltr.dataf <- ltr.dataf[ltr.dataf$revenue > 5000, ]
-  
- ## Save for future costs
-  
-### Comparison
-  
- ## Data prep
-    
-  # Re-order factors
-  abb.dataf$bedbath <- factor(abb.dataf$bedbath, 
-                              levels=c('2..2', '1..1', '2..1', '3..1', '3..2', '4..2'))
-  ltr.dataf$bedbath <- factor(ltr.dataf$bedbath, 
-                               levels=c('2..2', '1..1', '2..1', '3..1', '3..2', '4..2'))
-
-  # Add product as a variable
-  abb.dataf$product <- paste0(substr(abb.dataf$type, 1, 1), abb.dataf$bedbath)
-  ltr.dataf$product <- paste0(substr(ltr.dataf$type, 1, 1), ltr.dataf$bedbath)
-
-  # Convert suburbs from factor to character
-  abb.dataf$suburb <- as.character(abb.dataf$suburb)
-  ltr.dataf$suburb <- as.character(ltr.dataf$suburb)
 
  ## Global model
 
@@ -129,6 +66,8 @@
   ltr.mod.spec <- formula(log(event.price) ~ as.factor(bedbath) + as.factor(suburb))
   abb.mod.spec <- formula(log(nightly.rate) ~ as.factor(bedbath) + as.factor(suburb))
  
+  
+  
   glob <- fullMarketAnalysis(ltr.df=ltr.dataf,
                              abb.df=abb.dataf,
                              ltr.mod.spec=ltr.mod.spec,
@@ -136,15 +75,30 @@
                              exch.rate=exch.rate,
                              clip.field='suburb')
   
-  aa <- which(ltr.dataf$sub.mrkt=='city-core')
-  bb <- which(abb.dataf$sub.mrkt=='city-core')
-
-  beach <- fullMarketAnalysis(ltr.df=ltr.dataf[aa,],
-                             abb.df=abb.dataf[bb, ],
-                             ltr.mod.spec=ltr.mod.spec,
-                             abb.mod.spec=abb.mod.spec,
-                             exch.rate=exch.rate,
-                             clip.field='suburb')
+  ltr.type <- split(ltr.dataf, ltr.dataf$type)
+  abb.type <- split(abb.dataf, abb.dataf$type)
+  
+  apt <- fullMarketAnalysis(ltr.df=ltr.type[[1]],
+                            abb.df=abb.type[[1]],
+                                 ltr.mod.spec=ltr.mod.spec,
+                                 abb.mod.spec=abb.mod.spec,
+                                 exch.rate=exch.rate,
+                                 clip.field='suburb')
+  
+  
+  house <- fullMarketAnalysis(ltr.df=ltr.type[[2]],
+                              abb.df=abb.type[[2]],
+                              ltr.mod.spec=ltr.mod.spec,
+                              abb.mod.spec=abb.mod.spec,
+                              exch.rate=exch.rate,
+                              clip.field='suburb')
+  
+  ggplot(glob$abb, aes(x=longitude, y=latitude)) + 
+    geom_point(size=.1) + 
+    stat_bin2d(data=apt$abb, aes(alpha=..count.., fill=as.factor(abb.act)), 
+               binwidth=c(.005,.005)) + 
+    coord_cartesian(xlim=c(144.95,145.05), ylim=c(-37.75,-37.95))
   
   
   
+  ggplot(glob$abb, aes(x=longitude, y=latitude)) + geom_point(size=.1) + stat_bin2d(data=apt$abb, aes(alpha=..count.., fill=as.factor(abb.act)), binwidth=c(.005,.005)) + coord_cartesian(xlim=c(144.95,145.05), ylim=c(-37.75,-37.95))
