96092c6fc6719d0b7b7f886e31f68df07b676cfa
Andy Krause <andy.krause@unimelb.edu.au>
2017-01-26 21:14:10 +1100

    host classificatoin

--- abbReturns_Working.R
+++ abbReturns_Working.R
@@ -61,45 +61,114 @@
 ### Working analysis ---------------------------------------------------------------------  
   
   
-  calcBookStr <- function(id, book.data){
+  
+  
+  
+calcBookStr <- function(id, 
+                        book.data){
     
-    id.data <- book.data[book.data$property.id == id, 'status']
+  id.data <- book.data[book.data$property.id == id, 'status']
+    
+  if(length(id.data) > 1){
     
     # Divide by status and collapse
     status.string <- id.data[1]
     
-    for(ss in 2:length(b)){
+    for(ss in 2:length(id.data)){
       
-      if(b[jj] == b[[jj-1]]){
-        x <- paste0(x,b[[jj]])
+      if(id.data[ss] == id.data[[ss - 1]]){
+        status.string <- paste0(status.string, id.data[[ss]])
       } else {
-        x <- paste0(x,'.',b[[jj]])
+        status.string <- paste0(status.string, '.' ,id.data[[ss]])
       }
     }
     
-    c <- as.list(strsplit(x,'[.]')[[1]])
+    # Collapse into list objects
+    ss.list <- as.list(strsplit(status.string, '[.]')[[1]])
     
+    # Grab the first status of each
+    sg.obj <- substr(ss.list[[1]], 1, 1)
+    for(sg in 1:length(ss.list)){
+      sg.obj[sg] <- substr(ss.list[[sg]], 1, 1)  
+    }
     
+    # Find location of three types
+    id.B <- which(unlist(sg.obj) == 'B')
+    id.R <- which(unlist(sg.obj) == 'R')
+    id.A <- which(unlist(sg.obj) == 'A')
     
+    # Extract
+    if(length(id.R) > 0){
+      r.list <- ss.list[id.R]
+    }
+    if(length(id.A) > 0){
+      a.list <- ss.list[id.A]
+      avails <- unlist(lapply(a.list, nchar))
+      avail.rate <- sum(avails) / length(id.data)
+      
+    } else {
+      
+      avail.rate <- 0
     
-  }
+    }
+    
+    if(length(id.B) > 0){
+      b.list <- ss.list[id.B] 
+    
+      # Count longest and blocked times
+      blocks <- unlist(lapply(b.list, nchar))
+      
+      block.rate <- sum(blocks) / length(id.data)
+      longest.block <- max(blocks)
+      med.block <- median(blocks)
+      nbr.block <- length(id.B)
+      
+    } else {
+      
+      block.rate <- 0
+      longest.block <- 0
+      med.block <- 0
+      nbr.block <- 0
+      
+    }
+    
+    total.days <- length(id.data)  
+    
+  } else {
+    
+    block.rate <- NA
+    longest.block <- NA
+    med.block <- NA
+    nbr.block <- NA
+    total.days <- NA
+    
+  }  
+      
+  ## Return Values
   
+  return(data.frame(id=id,
+                    longest.block=longest.block,
+                    nbr.block=nbr.block,
+                    med.block=med.block,
+                    block.rate=block.rate,
+                    avail.rate=avail.rate,
+                    total.days=total.days))
+}
+
+
   
-  b <- daily.data[daily.data$property.id=='75924','status']
+  q <- lapply(as.list(abb.revs$property.id), calcBookStr, book.data=daily.data)
   
-  x <- b[1]
+  ww <- rbind.fill(q)
   
-  aa <- substr(c[[1]],1,1)
-  for(ii in 1:length(c)){
-    aa[ii] <- substr(c[[ii]],1,1)  
-  }
-  Q <- which(unlist(aa)=='B')
-  cc <- c[which(unlist(aa)=='B')]
-  longest.block <- max(unlist(lapply(cc, nchar)))
+  ww$nbrperdays <- ww$nbr.block / (ww$total.days/366)
   
+  ww$zbr <- (ww$block.rate-mean(ww$block.rate))/sd(ww$block.rate)
+  ww$znb <- (ww$nbrperdays-mean(ww$nbrperdays))/sd(ww$nbrperdays)
   
-  
-  
+  www <- ww[ww$total.days > 180, ]
+  rr <- kmeans(www[,c('znb', 'zbr')],3)
+  www$type <- rr$cluster
   
   
 
