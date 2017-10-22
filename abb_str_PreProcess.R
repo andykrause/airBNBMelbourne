7ed177580e142b9629d00f012b0dfe3f4c209b53
Andy Krause <andy@greenfieldadvisors.com>
2017-10-21 22:46:12 -0700

    Add: Update to include data up to Mar 2017

--- abb_str_PreProcess.R
+++ abb_str_PreProcess.R
@@ -6,80 +6,88 @@
 
  ## Load libraries
 
-  library(chron)
   library(plyr)
   library(stringr)
   library(lubridate)
   library(dplyr)
   library(multidplyr)
 
-  ## Set location
-
-  # Get computer names
-  comp.name <- Sys.info()['nodename']
+  ## Set locations
 
   # Assign path based on computer name
-  if(comp.name == '7020D-121777-W' | 
-     comp.name == 'DESKTOP-1D7JO4J' |
-     comp.name == 'GREENFIELD15'){
+  data_path <- 'c:/dropbox/research/airBNB/data/'
   
-    data.path <- 'c:/dropbox/research/airBNB/data/'
+  # Code Path
+  code_path <- getwd()
   
-  } else {
-  
-    data.path <- 'other path'
-  
-  }
-
  ## Load sources
 
-  source("c:/code/research/airbnbMelbourne/abb_Functions.R")
+  source(file.path(code_path, "abb_Functions.R"))
 
  ## Load Data  
    
   # Property Information
 
   str.data <- read.csv(file.path(data.path, '/raw/melbproperties.csv'), header=T)
-
+  str.data2 <- read.csv(file.path(data.path, '/raw/melbproperties_to_mar2017.csv'), 
+                        header=T)
+  
   # Daily rental information
 
   daily.data  <- read.csv(file.path(data.path, '/raw/melbDaily.csv'), header=T)
-
+  daily.data2  <- read.csv(file.path(data.path, '/raw/melbDaily_to_mar2017.csv'), header=T)
+  
  ## Fix Names
   
   names(str.data) <- tolower(names(str.data))
+  names(str.data2) <- tolower(names(str.data2))
   names(daily.data) <- tolower(names(daily.data))
-
+  names(daily.data2) <- tolower(names(daily.data2))
+  
+ ## Determine unique observations
+  
+  # Structure Data
+  str_df <- dplyr::bind_rows(str.data2, str.data)
+  str_df <- str_df %>% dplyr::filter(!duplicated(property.id))  
+  
+  # Daily Data
+  daily_df <- dplyr::bind_rows(daily.data2, daily.data) %>%
+    dplyr::mutate(uniq_id = paste0(property.id, '_', date)) %>%
+    dplyr::filter(!duplicated(uniq_id)) %>%
+    dplyr::select(property.id, date, status, price, booked.date, reservation.id)
+  
  ## Fix Dates  
   
   # Daily data
-  daily.data$date <- as.Date(daily.data$date)
-
+  daily_df$date <- lubridate::as_date(daily_df$date)
+  
   # Property level data
-  str.data$created.date <- as.Date(str.data$created.date)
-  str.data$created.year <- lubridate::year(str.data$created.date)
+  str_df$created.date <- lubridate::as_date(str_df$created.date)
+  str_df$created.year <- lubridate::year(str_df$created.date)
 
  ## Add Summary Information
 
   # Create new cluster
-  cluster <- get_default_cluster() 
+  abb_cl <- get_default_cluster() 
+  
   # Register function and variable
-  cluster_copy(cluster, abbCalcBookStr)
+  cluster_copy(abb_cl, abbCalcBookStr)
 
   # Summarize daily by property
-  daily.summ <- daily.data %>% 
-    multidplyr::partition(property.id, cluster=cluster) %>% 
+  daily_summ <- daily_df %>% 
+    multidplyr::partition(property.id, cluster=abb_cl) %>% 
     dplyr::do(abbCalcBookStr(.)) %>% 
     dplyr::collect()
   
  # Add summary data to property data
   
-  str.data <- merge(str.data, daily.summ, 
-                    by.x='property.id',
-                    by.y='id')
+  str_df <- str_df %>%
+    dplyr::inner_join(daily_summ, 
+                      by='property.id')
+  
 
  ## Write out data  
 
-  save(str.data, file=paste0(data.path, 'prepared/stpropdata.RData'))
-  save(daily.data, file=paste0(data.path, 'prepared/stdailydata.RData'))
+  save(str_df, file=paste0(data.path, 'prepared/stpropdata.RData'))
+  save(daily_df, file=paste0(data.path, 'prepared/stdailydata.RData'))
   
